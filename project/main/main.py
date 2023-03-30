b1d927c5da5952137b3e11b24e5c44ba4c9aae81
SheldonPhang <63786565+SheldonPhang@users.noreply.github.com>
2023-03-30 18:35:55 +0800

    Update main.py

--- project/main/main.py
+++ project/main/main.py
@@ -5,27 +5,28 @@ import multiprocessing
 import socket, socks
 import config_file as cfg_file
 import main.deal
-
+from concurrent.futures import ThreadPoolExecutor
 
 def fileDeal(target, name):
     file_object = open(target, 'r')
+    results = []
     try:
         lines = file_object.readlines()
-        pool = multiprocessing.Pool()
-        results = pool.map(name, [url.strip('\n') for url in lines])
-        pool.close()
-        pool.join()
-        # 将所有的结果展开成一个平坦的列表
-        res = [item for sublist in results for item in sublist]
-        return res
+        with ThreadPoolExecutor() as executor:
+            results = list(executor.map(lambda x: name(x, target), [url.strip('\n') for url in lines]))
+        return results
     except KeyboardInterrupt:
         print('\nCTRL+C 退出')
     finally:
         file_object.close()
 
-def urlDeal(target,name):
+#            results = list(executor.map(lambda x: name(x, target), [url.strip('\n') for url in lines]))
+
+
+
+def urlDeal(target, name):
     try:
-        res = name(target)
-        return res
+        return name(target)
     except KeyboardInterrupt:
         print('\nCTRL+C 退出')
+
